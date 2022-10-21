3bb99bc9d8c1990fb00d10b23aa105088c76623a
Robert <rguzman@graviti.mx>
2022-10-21 12:10:55 -0500

    useState, useEffect & fetch

--- firshITP/App.js
+++ firshITP/App.js
@@ -1,14 +1,32 @@
-import React from "react";
-import {TouchableOpacity, View} from "react-native";
-import HolaMundo from "./src/componets/HolaMundo";
-import Title from "./src/componets/Title";
-import styles from "./src/styles/styles";
+import React, {useEffect, useState} from 'react';
+import {TouchableOpacity, View} from 'react-native';
+import {Button, Input} from '@rneui/themed';
+import HolaMundo from './src/componets/HolaMundo';
+import Title from './src/componets/Title';
+import styles from './src/styles/styles';
+import {getText} from './src/services/getData';
 
 const App = () => {
+  const [text, setTexto] = useState('');
+  const [fact, setFact] = useState('');
+
+  useEffect(() => {
+    getText().then(result => {
+      console.log(result, result.fact);
+      setFact(result.fact);
+    });
+  }, []);
+
   return (
     <View style={styles.main}>
+      <Button title="Solid" />
+      <Button title="Outline" type="outline" />
+      <Button title="Clear" type="clear" />
+
+      <Input placeholder="BASIC INPUT" value={text} onChangeText={setTexto} />
+
       <HolaMundo
-        parametro1="hola"
+        parametro1={text}
         propiedad2="red"
         fontSize={18}
         value={true}
@@ -22,7 +40,7 @@ const App = () => {
         />
       </TouchableOpacity>
       <HolaMundo
-        parametro1="qué tal!!!"
+        parametro1={fact}
         propiedad2="green"
         fontSize={28}
         value={false}
