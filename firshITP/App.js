8beffe1c9f4d4a8483433e7516e83f24ba258b09
Robert <rguzman@graviti.mx>
2022-10-28 17:35:11 -0500

    import img

--- firshITP/App.js
+++ firshITP/App.js
@@ -1,51 +1,17 @@
-import React, {useEffect, useState} from 'react';
-import {TouchableOpacity, View} from 'react-native';
-import {Button, Input} from '@rneui/themed';
-import HolaMundo from './src/componets/HolaMundo';
-import Title from './src/componets/Title';
+import React from 'react';
+import {View, ActivityIndicator} from 'react-native';
+import {Image} from '@rneui/themed';
 import styles from './src/styles/styles';
-import {getText} from './src/services/getData';
+import img1 from './src/assets/img1.jpeg';
 
 const App = () => {
-  const [text, setTexto] = useState('');
-  const [fact, setFact] = useState('');
-
-  useEffect(() => {
-    getText().then(result => {
-      console.log(result, result.fact);
-      setFact(result.fact);
-    });
-  }, []);
-
   return (
     <View style={styles.main}>
-      <Button title="Solid" />
-      <Button title="Outline" type="outline" />
-      <Button title="Clear" type="clear" />
-
-      <Input placeholder="BASIC INPUT" value={text} onChangeText={setTexto} />
-
-      <HolaMundo
-        parametro1={text}
-        propiedad2="red"
-        fontSize={18}
-        value={true}
+      <Image
+        source={img1}
+        containerStyle={styles.item}
+        PlaceholderContent={<ActivityIndicator />}
       />
-      <TouchableOpacity>
-        <HolaMundo
-          parametro1="adios"
-          propiedad2="blue"
-          fontSize={24}
-          value={true}
-        />
-      </TouchableOpacity>
-      <HolaMundo
-        parametro1={fact}
-        propiedad2="green"
-        fontSize={28}
-        value={false}
-      />
-      <Title />
     </View>
   );
 };
