dd16315af1c58c775a5c0acd4c3c431ff304f0ff
Robert <rguzman@graviti.mx>
2022-10-28 18:10:05 -0500

    fead - utilizar api para obtener texto

--- firshITP/App.js
+++ firshITP/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, ActivityIndicator, Text} from 'react-native';
 import {Image, Button} from '@rneui/themed';
 import styles from './src/styles/styles';
 import img1 from './src/assets/img1.jpeg';
+import {getText} from './src/services/getData';
 
 const App = () => {
+  const [txt, setTxt] = useState('Valor por default');
+
+  useEffect(() => {
+    updateTxt();
+  }, []);
+
+  const updateTxt = () => getText().then(result => setTxt(result.fact));
+
   return (
     <View style={styles.main}>
       <View style={styles.left}>
-        <Button title="Actualizar" />
+        <Button title="Actualizar" onPress={updateTxt} />
       </View>
       <Image
         source={img1}
         containerStyle={styles.item}
         PlaceholderContent={<ActivityIndicator />}
       />
-      <Text style={styles.txt}>
-        kadfjasdfñlkasjd flñakjfañlkdj asdñlfkjasdlñfkajsd adsñflkajsdñflkajdfa
-      </Text>
+      <Text style={styles.txt}>{txt}</Text>
     </View>
   );
 };
