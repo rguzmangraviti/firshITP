f07aa15d4ae0d4ebd424dc0f63c0aeb0402ce618
Robert <rguzman@graviti.mx>
2022-10-28 17:53:20 -0500

    fead - maqueta

--- firshITP/App.js
+++ firshITP/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import {View, ActivityIndicator} from 'react-native';
-import {Image} from '@rneui/themed';
+import {View, ActivityIndicator, Text} from 'react-native';
+import {Image, Button} from '@rneui/themed';
 import styles from './src/styles/styles';
 import img1 from './src/assets/img1.jpeg';
 
 const App = () => {
   return (
     <View style={styles.main}>
+      <View style={styles.left}>
+        <Button title="Actualizar" />
+      </View>
       <Image
         source={img1}
         containerStyle={styles.item}
         PlaceholderContent={<ActivityIndicator />}
       />
+      <Text style={styles.txt}>
+        kadfjasdfñlkasjd flñakjfañlkdj asdñlfkjasdlñfkajsd adsñflkajsdñflkajdfa
+      </Text>
     </View>
   );
 };
--- firshITP/src/styles/styles.js
+++ firshITP/src/styles/styles.js
@@ -1,5 +1,8 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 import {background, primary} from './colors';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   main: {
@@ -8,12 +11,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: background,
   },
+  left: {
+    width,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    margin: 15,
+    padding: 15,
+  },
   textPrimary: {
     color: primary,
   },
   title: {
     color: primary,
     fontSize: 18,
+  },
+  item: {
+    width: width - 30,
+    height: width - 30,
+  },
+  txt: {
+    fontSize: 18,
+    width: width - 30,
   },
 });
 
